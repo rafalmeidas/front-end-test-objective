9557477bc0103f624e360b55ac7eab830d4afe65
rafalmeidas <rafalmeidas21@gmail.com>
2023-10-07 23:09:32 -0300

    feat: add container component

---
+++ src/layout/container/container.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+
+import Header from "../header/header";
+
+import styles from "./container.module.scss";
+
+export default function Container() {
+  return (
+    <>
+      <Header />
+      <main className={styles.wrapper}>
+        <Outlet />
+      </main>
+    </>
+  );
+}
