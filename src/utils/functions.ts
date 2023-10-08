1f6b7b627f4d9c72373f901327c840628a0c2841
rafalmeidas <rafalmeidas21@gmail.com>
2023-10-07 23:07:59 -0300

    feat: add functions utils

---
+++ src/utils/functions.ts
@@ -0,0 +1,27 @@
+import md5 from "md5";
+
+import { Thumbnail } from "../types/marvels";
+
+export const getHash = (
+  timeStamp: string,
+  privateKey: string,
+  publicKey: string
+) => md5(timeStamp + privateKey + publicKey);
+
+export const getTimeStamp = () => Date.now().toString();
+
+export const handleResponse = async <T>(response: Response) => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  const data = await response.json();
+
+  return data.data as T;
+};
+
+export const generateThumbnail = (thumb: Thumbnail): string => {
+  if (!thumb) return "";
+
+  return `${thumb.path}.${thumb.extension}`;
+};
