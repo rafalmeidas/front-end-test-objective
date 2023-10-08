552c236123a04642af21ff856bed870bea7c0046
rafalmeidas <rafalmeidas21@gmail.com>
2023-10-07 23:05:31 -0300

    feat: add page numbers component

---
+++ src/components/page-numbers/page-numbers.tsx
@@ -0,0 +1,29 @@
+import usePaginator from "../../hooks/usePaginator";
+
+import styles from "./page-numbers.module.scss";
+
+type PageNumbersProps = {
+  onChangePage: (page: number) => void;
+};
+
+export default function PageNumbers({ onChangePage }: PageNumbersProps) {
+  const { pages, currentPage } = usePaginator();
+
+  return (
+    <ul className={styles.wrapper_page_numbers}>
+      {pages.map((page) => (
+        <li key={`page-${page}`}>
+          <button
+            className={`${styles.buttonPage} ${
+              currentPage === page ? styles.selected : styles.unselected
+            }`}
+            onClick={() => onChangePage(page)}
+            aria-label={`mudar para a página ${page}`}
+          >
+            {page}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
